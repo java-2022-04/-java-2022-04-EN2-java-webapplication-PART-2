caa4d4bdfe361b1ac8e8d12532b2e888a2880dce
Erik Hoelzel <erik.hoelzel@posteo.de>
2022-11-08 15:36:25 +0100

    initial commit of first repository

---
+++ src/main/java/com/brightslearning/webapp/SpringDataAppDemo/Student.java
@@ -0,0 +1,30 @@
+package com.brightslearning.webapp.SpringDataAppDemo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
