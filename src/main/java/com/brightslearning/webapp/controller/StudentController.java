df646a72ff04407a4e98027438a666f050c8357b
Erik Hoelzel <erik.hoelzel@posteo.de>
2022-11-13 19:47:22 +0100

    makes simple use of a model to display it within the html

--- src/main/java/com/brightslearning/webapp/controller/StudentController.java
+++ src/main/java/com/brightslearning/webapp/controller/StudentController.java
@@ -6,6 +6,7 @@ import com.brightslearning.webapp.service.StudentHTMLService;
 import com.brightslearning.webapp.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,8 +31,9 @@ public class StudentController {
         this.searchService = searchService;
     }
 
-    @GetMapping("/")
-    public String index() {
+    @GetMapping("/") //http://localhost:8080/?name=Bob
+    public String index(Model model, @RequestParam String name) {
+        model.addAttribute("name", name);
         return "index";
     }
 
